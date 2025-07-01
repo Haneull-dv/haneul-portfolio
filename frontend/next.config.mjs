a6dbc7f8c1460ad3e30f90b2966a5bbecdd64be9
haneull.dv <haneull.dv@gmail.com>
2025-07-01 20:55:33 +0900

    disclosure 완벽

---
+++ frontend/next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+ 
