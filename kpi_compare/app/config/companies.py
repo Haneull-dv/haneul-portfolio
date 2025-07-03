74d1933375b1a1825d4f84332d03212bb9346139
haneull.dv <haneull.dv@gmail.com>
2025-07-03 17:08:20 +0900

    게임회사 추가

--- kpi_compare/app/config/companies.py
+++ kpi_compare/app/config/companies.py
@@ -2,8 +2,6 @@
 KPI 비교 서비스에서 지원하는 기업 목록
 """
 SUPPORTED_COMPANIES = [
-     {'corp_name': '네이버', 'corp_code': '00266961'},
-    {'corp_name': '카카오', 'corp_code': '00918444'},
     {'corp_name': '크래프톤', 'corp_code': '00760971'},
     {'corp_name': '엔씨소프트', 'corp_code': '00261443'},
     {'corp_name': '넷마블', 'corp_code': '00904672'},
@@ -21,5 +19,19 @@ SUPPORTED_COMPANIES = [
     {'corp_name': '썸에이지', 'corp_code': '01092901'}, # 넥써쓰 -> 썸에이지
     {'corp_name': '시프트업', 'corp_code': '01384787'},
     {'corp_name': '컴투스', 'corp_code': '00476498'},
-    {'corp_name': '스마일게이트엔터테인먼트', 'corp_code': '00809049'}
+    {'corp_name': '스마일게이트엔터테인먼트', 'corp_code': '00809049'},
+    {'corp_name': '네오위즈홀딩스', 'corp_code': '00266952'},
+    {'corp_name': '더블유게임즈', 'corp_code': '01010110'},
+    {'corp_name': '위메이드맥스', 'corp_code': '00643656'},
+    {'corp_name': '데브시스터즈', 'corp_code': '01008762'},
+    {'corp_name': '웹젠', 'corp_code': '00405320'},
+    {'corp_name': '넵튠', 'corp_code': '01067808'},
+    {'corp_name': '컴투스홀딩스', 'corp_code': '00535746'},
+    {'corp_name': '고스트스튜디오', 'corp_code': '01416235'},
+    {'corp_name': '엠게임', 'corp_code': '00397058'},
+    {'corp_name': '위메이드플레이', 'corp_code': '00815767'},
+    {'corp_name': '라인게임즈', 'corp_code': '01246894'},
+    {'corp_name': '넥써쓰', 'corp_code': '01042429'},
+    {'corp_name': '한빛소프트', 'corp_code': '00348292'},
+    {'corp_name': '스타코링크', 'corp_code': '00373571'}
 ]
